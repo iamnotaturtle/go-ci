eda1703c181c628ed1b9c570834e98a7ce35ca4b
github-action-benchmark <github@users.noreply.github.com>
2021-11-29 23:17:23 +0000

    add Benchmark Results (go) benchmark result for 292ad9a78d9331617860aee4e1c55394a9a512f6

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1638227843580,
+  "repoUrl": "https://github.com/iamnotaturtle/go-ci",
+  "entries": {
+    "Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "iamnotaturtle@users.noreply.github.com",
+            "name": "Yakov",
+            "username": "iamnotaturtle"
+          },
+          "committer": {
+            "email": "iamnotaturtle@users.noreply.github.com",
+            "name": "Yakov",
+            "username": "iamnotaturtle"
+          },
+          "distinct": true,
+          "id": "292ad9a78d9331617860aee4e1c55394a9a512f6",
+          "message": "Clear out go.mod changes",
+          "timestamp": "2021-11-29T18:16:57-05:00",
+          "tree_id": "a2fd68de9eaea017d9c6266e18830f67fa3a147b",
+          "url": "https://github.com/iamnotaturtle/go-ci/commit/292ad9a78d9331617860aee4e1c55394a9a512f6"
+        },
+        "date": 1638227842735,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetCharacter",
+            "value": 1876,
+            "unit": "ns/op\t    1410 B/op\t      11 allocs/op",
+            "extra": "541244 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
